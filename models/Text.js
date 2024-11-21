49b547b6a924f5b03129806747f441f3263a9f94
nguyennhuthang25121974@gmail.com <nguyennhuthang25121974@gmail.com>
2024-11-21 09:45:44 +0700

    Fix DELETE /delete route and frontend logic

--- models/Text.js
+++ models/Text.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+
 const textSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-});
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+}, { timestamps: true }); // Adds createdAt and updatedAt fields
+
 module.exports = mongoose.model('Text', textSchema);
